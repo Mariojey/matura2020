d317c45ba8b5d60b6563c400ad5543d1189c2af8
Mariusz Jacek <marios.yaceg@gmail.com>
2022-08-13 11:23:12 +0200

    test

--- 4.2.py
+++ 4.2.py
@@ -11,8 +11,8 @@ tab_word = ""
 for i in text_lines:
     tab_word = i.split(" ")[1]
     #pobranie elementu io indexie 1
-    l_string_char = ""
-    c_string_char = ""
+    l_string_char = "" #najdłuższy ciąg znaków
+    c_string_char = "" #autalnie badany ciąg znaków
     for a in range(len(tab_word)):
         if len(c_string_char) == 0 or c_string_char[0] == tab_word[a]:
             c_string_char += tab_word[a]
